ba6883fcabed3dfd9c060e4f98fca66aee1d39b5
wttch（王冲） <wttch@wttch.com>
2022-05-19 23:35:54 +0800

    transaction

---
+++ SwiftUI Bootcamp/TransitionBootcamp.swift
@@ -0,0 +1,44 @@
+//
+//  TransitionBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Wttch on 2022/5/19.
+//
+
+import SwiftUI
+
+struct TransitionBootcamp: View {
+    @State var showView: Bool = false
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            VStack {
+                Button("Button") {
+                    withAnimation(.spring()) {
+                        showView.toggle()
+                    }
+                }
+                Spacer()
+            }
+            
+            if showView {
+                RoundedRectangle(cornerRadius: 30)
+                    // 屏幕高度的一半
+                    .frame(height: UIScreen.main.bounds.height * 0.5)
+                    // 从一个方向出来再返回
+                    // .transition(.move(edge: .bottom))
+                    // 不透明度
+                    // .transition(.opacity)
+                    // 可以组合出现和消失使用不同的动画,不知道为啥不生效
+                    .transition(.asymmetric(insertion: .move(edge: .leading), removal: .move(edge: .bottom)))
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+struct TransitionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TransitionBootcamp()
+    }
+}
