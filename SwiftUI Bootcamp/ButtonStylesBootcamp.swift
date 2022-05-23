1066a560e3c569e0748ab24ebb788ab282402d24
wttch（王冲） <wttch@wttch.com>
2022-05-23 23:40:04 +0800

    button styles

---
+++ SwiftUI Bootcamp/ButtonStylesBootcamp.swift
@@ -0,0 +1,75 @@
+//
+//  ButtonStylesBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Wttch on 2022/5/23.
+//
+
+import SwiftUI
+
+struct ButtonStylesBootcamp: View {
+    var body: some View {
+        VStack {
+            Button("bordered") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.bordered)
+            Button("automatic") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.automatic)
+            Button("plain") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.plain)
+            Button("borderless") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderless)
+            Button("borderedProminent") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            Button("borderedProminent") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.regular)
+            Button("borderedProminent") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.mini)
+            Button("borderedProminent") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.small)
+        }
+        .padding()
+    }
+}
+
+struct ButtonStylesBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonStylesBootcamp()
+            .preferredColorScheme(.dark)
+    }
+}
